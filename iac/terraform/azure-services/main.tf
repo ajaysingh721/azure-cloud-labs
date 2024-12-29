fe864a7c647b3eb1a31a8b7b05e6753cfaf9014c
Ajay Singh <ajay.singh721@gmail.com>
2024-12-29 16:31:39 +0530

    Add Azure Storage Container and Key Vault resources; update workflow name

--- iac/terraform/azure-services/main.tf
+++ iac/terraform/azure-services/main.tf
@@ -12,5 +12,23 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
 
+resource "azurerm_storage_container" "sc" {
+  name                  = "${var.prefix}sc"
+  storage_account_id    = azurerm_storage_account.sa.id
+  container_access_type = "private"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                            = "${var.prefix}kv"
+  resource_group_name             = var.rg
+  location                        = var.location
+  sku_name                        = "Standard"
+  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days      = 7
+  purge_protection_enabled        = false
+  enabled_for_disk_encryption     = true
+  enabled_for_deployment          = true
+  enabled_for_template_deployment = true
 }
