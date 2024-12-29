b8b066619d21ae1245b23706300653b5a1620975
Ajay Singh <ajay.singh721@gmail.com>
2024-12-29 14:40:26 +0530

    Simplify backend configuration in Terraform by removing redundant resource group and storage account settings

--- iac/terraform/azure-services/providers.tf
+++ iac/terraform/azure-services/providers.tf
@@ -8,10 +8,7 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "1-1899c343-playground-sandbox"
-    storage_account_name = "tfstate1899c343"
-    container_name       = "terraform-state"
-    key                  = "terraform.tfstate"
+    key = "terraform.tfstate"
   }
 
 }
