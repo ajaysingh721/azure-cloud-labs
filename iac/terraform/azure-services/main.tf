d15899a07340fcce740bf6dfff7d03ed97c84455
Ajay Singh <ajay.singh721@gmail.com>
2024-12-30 11:54:21 +0530

    Add Azure App Service and App Service Plan resources to Terraform configuration

--- iac/terraform/azure-services/main.tf
+++ iac/terraform/azure-services/main.tf
@@ -6,29 +6,30 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "${var.prefix}sa1231"
-  resource_group_name      = var.rg
-  location                 = var.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+resource "azurerm_app_service_plan" "web" {
+  name                = "${var.prefix}web"
+  location            = var.location
+  resource_group_name = var.rg
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
 
-resource "azurerm_storage_container" "sc" {
-  name                  = "${var.prefix}sc"
-  storage_account_id    = azurerm_storage_account.sa.id
-  container_access_type = "private"
-}
+resource "azurerm_app_service" "app" {
+  name                = "${var.prefix}app"
+  location            = var.location
+  resource_group_name = var.rg
+  app_service_plan_id = azurerm_app_service_plan.web.id
 
-resource "azurerm_key_vault" "kv" {
-  name                            = "${var.prefix}kv"
-  resource_group_name             = var.rg
-  location                        = var.location
-  sku_name                        = "standard"
-  tenant_id                       = var.tenant_id
-  soft_delete_retention_days      = 7
-  purge_protection_enabled        = false
-  enabled_for_disk_encryption     = true
-  enabled_for_deployment          = true
-  enabled_for_template_deployment = true
+  site_config {
+    linux_fx_version = "DOCKER|nginx"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
 }
