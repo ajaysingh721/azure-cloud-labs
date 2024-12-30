aa4ab793bfa93f426dc949c11794cc7fd80319bb
Ajay Singh <ajay.singh721@gmail.com>
2024-12-30 12:03:23 +0530

    Refactor Azure App Service Plan and update to Linux Web App configuration in Terraform

--- iac/terraform/azure-services/main.tf
+++ iac/terraform/azure-services/main.tf
@@ -6,30 +6,32 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
+
 resource "azurerm_app_service_plan" "asp" {
   name                = "${var.prefix}asp"
-  location            = var.location
   resource_group_name = var.rg
+  location            = var.location
   kind                = "Linux"
   reserved            = true
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Basic"
+    size = "B1"
   }
 }
 
-resource "azurerm_app_service" "app" {
-  name                = "${var.prefix}app"
-  location            = var.location
+resource "azurerm_linux_web_app" "lwa" {
+
+  name                = "${var.prefix}lwa"
   resource_group_name = var.rg
-  app_service_plan_id = azurerm_app_service_plan.asp.id
-
-  site_config {
-    linux_fx_version = "DOCKER|nginx"
-  }
-
+  location            = var.location
+  service_plan_id     = azurerm_app_service_plan.asp.id
+  https_only          = true
   app_settings = {
     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+
+  site_config {
+    linux_fx_version = "DOCKER|${azurerm_container_registry.acr.login_server}/app:latest"
   }
 }
