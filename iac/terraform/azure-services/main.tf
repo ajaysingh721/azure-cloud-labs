90625db875fabce81e1c4a53c64da9eb9defd9ac
Ajay Singh <ajay.singh721@gmail.com>
2025-01-01 11:42:06 +0530

    Remove container group resource and associated credentials from Terraform configuration

--- iac/terraform/azure-services/main.tf
+++ iac/terraform/azure-services/main.tf
@@ -16,43 +16,12 @@ resource "azurerm_container_registry" "acr" {
   location            = data.azurerm_resource_group.rg.location
   sku                 = "Standard"
   admin_enabled       = true
+  admin_username      = "acladmin"
+  admin_password      = "P@ssw0rd1234!"
 
   tags = {
     environment = "dev"
   }
-}
-
-resource "azurerm_container_group" "container_group" {
-  name                = var.aci_name
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = data.azurerm_resource_group.rg.location
-  os_type             = "Linux"
-
-  container {
-    name   = "node-app"
-    image  = "${azurerm_container_registry.acr.login_server}/${var.acr_image_name}:latest"
-    cpu    = "0.5"
-    memory = "1.5"
-
-    ports {
-      port     = 8080
-      protocol = "TCP"
-    }
-  }
-
-  image_registry_credential {
-    server   = azurerm_container_registry.acr.login_server
-    username = azurerm_container_registry.acr.admin_username
-    password = azurerm_container_registry.acr.admin_password
-  }
-  restart_policy = "OnFailure"
-
-  lifecycle {
-    ignore_changes = [
-      "container.0.image",
-    ]
-  }
-  depends_on = [azurerm_container_registry.acr]
 }
 
 resource "azurerm_service_plan" "asp" {
