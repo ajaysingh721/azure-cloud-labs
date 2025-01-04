6ff4c81a63ae884a8d4b61b0e8045062619d0e3a
Ajay Singh <ajay.singh721@gmail.com>
2025-01-04 23:17:57 +0530

    Update storage account location from westus to eastus in terraform-setup.sh

--- iac/cloud-shell/terraform-setup.sh
+++ iac/cloud-shell/terraform-setup.sh
@@ -8,7 +8,7 @@ CONTAINER_NAME="terraform-state"
 #az group create --name $RESOURCE_GROUP_NAME --location eastus
 
 # Create a storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob --location westus
+az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --encryption-services blob --location eastus
 
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME
