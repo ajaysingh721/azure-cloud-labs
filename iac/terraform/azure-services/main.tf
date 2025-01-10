860dfce742b90c48eded609440f7976f1b92394c
Ajay Singh <ajay.singh721@gmail.com>
2025-01-10 21:47:02 +0530

    Update web app names in Terraform configuration for development and QA environments

--- iac/terraform/azure-services/main.tf
+++ iac/terraform/azure-services/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_linux_web_app" "lwa" {
 }
 
 resource "azurerm_linux_web_app" "lwadev" {
-  name                = "${var.web_app_name}-dev_01"
+  name                = "dev-${var.web_app_name}"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.asp.id
@@ -113,7 +113,7 @@ resource "azurerm_linux_web_app" "lwadev" {
 }
 
 resource "azurerm_linux_web_app" "lwaqa" {
-  name                = "${var.web_app_name}-qa"
+  name                = "qa-${var.web_app_name}"
   resource_group_name = data.azurerm_resource_group.rg.name
   location            = data.azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.asp.id
